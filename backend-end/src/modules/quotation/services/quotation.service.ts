8599ff6c8dd8a8c80614fc10390931c81cbcb348
shiyuq <1543082564@qq.com>
2025-11-17 19:53:11 +0800

    优化

--- backend-end/src/modules/quotation/services/quotation.service.ts
+++ backend-end/src/modules/quotation/services/quotation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import _ from 'lodash';
 import ExcelJS from 'exceljs';
+import type { Anchor } from 'exceljs';
 import child_process from 'child_process';
 import path from 'path';
 import sizeOf from 'image-size';
@@ -47,48 +48,20 @@ export class QuotationService {
     const workbook = new ExcelJS.Workbook();
     const sheet = workbook.addWorksheet('quotation');
 
-    // 表头
-    sheet.columns = [
-      { header: 'PICTURE', key: 'picture', width: 25 },
-      { header: 'PRODUCT', key: 'product', width: 20 },
-      { header: 'SKU', key: 'sku', width: 20 },
-      { header: 'SPECIFICATION', key: 'desc', width: 25 },
-      { header: 'Size', key: 'size', width: 25 },
-      { header: 'QTY', key: 'qty', width: 15 },
-      { header: 'UNIT PRICE', key: 'unitPrice', width: 25 },
-      { header: 'TOTAL PRICE', key: 'totalPrice', width: 25 },
-    ];
+    // 设置表头
+    this.setHeader(sheet);
 
-    sheet.getRow(1).height = 45;
-    sheet.getRow(1).eachCell((cell) => {
-      cell.fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'FF000000' }, // 黑色背景
-      };
-      cell.font = {
-        name: 'Tahoma Regular',
-        size: 14,
-        color: { argb: 'FFFFFFFF' }, // 白色字体
-        bold: true,
-      };
-      cell.alignment = { vertical: 'middle', horizontal: 'center' };
-      cell.border = {
-        top: { style: 'thin' },
-        left: { style: 'thin' },
-        bottom: { style: 'thin' },
-        right: { style: 'thin' },
-      };
-    });
-
+    // 全局变量，设置当前行和图片的宽度
     let currentRow = 2;
+    let minRowHeight = 33;
+
     for (const productId of Object.keys(skuMap)) {
       // 产品信息、sku信息
       const productInfo = productMap[productId];
       const skus = skuMap[productId];
       const imageId = skus[0].imageId;
 
-      const rowHeight = skus.length === 1 ? 66 : 33;
+      const rowHeight = skus.length === 1 ? minRowHeight * 2 : minRowHeight;
       const pictureStartRow = currentRow;
       const pictureEndRow = currentRow + skus.length - 1;
 
@@ -96,16 +69,25 @@ export class QuotationService {
         const row = sheet.getRow(currentRow);
         row.height = rowHeight;
         row.getCell('sku').value = sku.skuCode;
+
         row.getCell('desc').value =
           sku.pricingType === PricingType.PriceByAttribute
             ? sku.desc
             : productInfo.desc;
+
         row.getCell('size').value =
           sku.pricingType === PricingType.PriceByAttribute
             ? sku.attributeValue
             : productInfo.desc;
+        if (sku.pricingType === PricingType.PriceByAttribute) {
+          row.getCell('size').numFmt = `0"${sku.unit}"`;
+        }
+
         row.getCell('qty').value = countMap[sku.skuCode].quantity;
-        row.getCell('unitPrice').value = sku.unitPrice;
+        row.getCell('qty').numFmt = '0"pc"';
+        // row.getCell('unitPrice').value = sku.unitPrice;
+        row.getCell('cost').value = sku.unitPrice;
+        row.getCell('unitPrice').numFmt = `0.00"/${sku.unit}"`;
         row.getCell('totalPrice').value = {
           formula:
             sku.pricingType === PricingType.PriceByAttribute
@@ -145,13 +127,18 @@ export class QuotationService {
         }),
         {
           tl: {
-            col: 0,
-            row: pictureStartRow - 1,
-            nativeColOff: 10,
-            nativeRowOff: 10,
-          },
+            nativeRow: pictureStartRow - 1, // 行索引从0开始
+            nativeCol: 0, // 列索引从0开始
+            nativeRowOff:
+              9525 * Math.floor((rowHeight * skus.length * 1.33 - height) / 4),
+            nativeColOff:
+              9525 *
+              Math.floor(
+                ((sheet.getColumn('picture').width ?? 25) * 7 - width) / 2,
+              ),
+          } as Anchor,
           ext: { width, height },
-          editAs: 'oneCell',
+          editAs: 'absolute',
         },
       );
     }
@@ -175,5 +162,46 @@ export class QuotationService {
     const scaledWidth = origW * scale;
 
     return { width: scaledWidth, height: scaledHeight };
+  }
+
+  private setHeader(sheet: ExcelJS.Worksheet) {
+    // 表头
+    sheet.columns = [
+      { header: 'PICTURE', key: 'picture', width: 25 },
+      { header: 'PRODUCT', key: 'product', width: 20 },
+      { header: 'SKU', key: 'sku', width: 20 },
+      { header: 'SPECIFICATION', key: 'desc', width: 25 },
+      { header: 'Size', key: 'size', width: 25 },
+      { header: 'QTY', key: 'qty', width: 15 },
+      { header: 'UNIT PRICE', key: 'unitPrice', width: 25 },
+      { header: 'TOTAL PRICE', key: 'totalPrice', width: 25 },
+      { header: '成本', key: 'cost', width: 25 },
+    ];
+
+    sheet.getRow(1).height = 45;
+    sheet.getRow(1).eachCell((cell) => {
+      cell.fill = {
+        type: 'pattern',
+        pattern: 'solid',
+        fgColor: { argb: 'FF000000' }, // 黑色背景
+      };
+      cell.font = {
+        name: 'Tahoma Regular',
+        size: 14,
+        color: { argb: 'FFFFFFFF' }, // 白色字体
+        bold: true,
+      };
+      cell.alignment = {
+        wrapText: true,
+        vertical: 'middle',
+        horizontal: 'center',
+      };
+      cell.border = {
+        top: { style: 'thin' },
+        left: { style: 'thin' },
+        bottom: { style: 'thin' },
+        right: { style: 'thin' },
+      };
+    });
   }
 }
